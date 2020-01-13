f5639b43b8b1ceb1b9e94072893590f681805eed
Victoria Curl <victoriacurl9@gmail.com>
2020-01-13 14:49:58 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -12,19 +12,19 @@ class Dog
   end
 end
 
-class Dog
-  def initialize(breed)
-    @breed = breed
-  end
-
-  def breed=(breed)
-    @breed = breed
-  end
-
-  def breed
-    @breed
-  end
-end
+# class Dog
+#   def initialize(breed)
+#     @breed = breed
+#   end
+# 
+#   def breed=(breed)
+#     @breed = breed
+#   end
+# 
+#   def breed
+#     @breed
+#   end
+# end
 
 fido = Dog.new("Fido")
 fido.name
